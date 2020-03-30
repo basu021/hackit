6fc1e8e9bca625f948e81352e324c5114327f80c
basu021 <57030150+basu021@users.noreply.github.com>
2020-03-30 12:58:03 +0530

    Add files via upload

---
+++ r-install.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+clear
+apt install figlet -y
+figlet -f small Installing...
+sleep 1s
+figlet -f small please wait
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat -y
+echo -e "\e[35mCompleted"
+figlet -f big Done !!! |lolcat
+echo -e "\e[93mNow run ⟩bash run.sh\e[0m...!!!"
+echo ''
+echo -e "\e[1m\e[32m Devlope4d by :\e[33m Instagram- @basudevrout2001"
+echo ''
+echo ''
