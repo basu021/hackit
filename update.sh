f229e7da2bdbb250ba7278aa3e7c28a13a8fc013
basu021 <57030150+basu021@users.noreply.github.com>
2020-03-31 18:02:34 +0530

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd
+
+figlet -f small please wait ... |lolcat
+
+
+rm -rf hackit
+
+git clone https://github.com/basu021/hackit.git
+
+cd hackit
+
+chmod +x *
+
+sleep 1s
+
+figlet done
+
+bash run.sh
