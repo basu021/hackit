18fa35f01ced4b075fd940f988e36602d577403f
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-19 23:16:39 +0530

    Add files via upload

---
+++ fix.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#code by - @basudevrout2001
+#follow_me_on_insta
+
+clear
+
+echo -e "\e[38;5;9mFixing issue .\e[0m"
+
+sleep 1s
+clear
+
+echo -e "\e[38;5;9mFixing issue ..\e[0m"
+
+sleep 1s
+clear
+
+echo -e "\e[38;5;9mFixing issue ...\e[0m"
+
+sleep 1s
+
+figlet -f small Wait |lolcat
+
+rm ngrok-stable-linux-arm.zip.1
+
+sleep 0.5s
+
+echo please wait !!!|lolcat
+
+sleep 0.3s
+
+echo "█████▓...7% completed"
+sleep 0.3s
+echo " "
+echo "██████████...18% completed"
+
+sleep 0.3s
+echo " "
+echo "████████████████...46% completed"
+sleep 0.5s
+echo " "
+echo "████████████████████████...78% completed"
+sleep 0.3s
+echo " "
+echo "████████████████████████████...83% completed"
+sleep 0.4s
+echo " "
+echo "█████████████████████████████████...93% completed"
+sleep 0.9s
+echo " "
+echo "██████████████████████████████████████...100% completed"
+sleep 0.7s
+echo " "
+echo -e "\e[38;5;120m Done...!\e[0m"
+
+sleep 3s
+echo " "
+
+echo -e "\e[93mNow run ⟩bash run.sh\e[0m...!!!"
